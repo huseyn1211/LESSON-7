93037c44da8d2f0198b99289191d908d14e8e073
huseyn1211 <quluzadeh43@gmail.com>
2024-11-11 18:11:34 +0400

    homework

---
+++ app9.js
@@ -0,0 +1,92 @@
+function numb(numb) {
+    console.log(1200 + (75 * numb))
+}
+numb(20)
+
+
+
+let a = prompt("reqemi daxil edin")
+a.split("")
+Number(a[0])
+Number(a[1])
+
+
+function numberToText(c) {
+    if (c == 1 ) {
+        console.log("on")
+    }
+
+
+    else if (c == 2) {
+        console.log("iyirmi")
+    }
+    else if (c == 3) {
+        console.log("otuz")
+    }
+    else if (c == 4) {
+        console.log("qirx")
+
+    } else if (c == 5) {
+        console.log("elli")
+
+    } else if (c == 6) {
+        console.log("altmis")
+    } else if (c == 7) {
+        console.log("yetmis")
+    }
+    else if (c == 8) {
+        console.log("seksen")
+    } else if (c == 9) {
+        console.log("doxsan")
+    }
+    else{
+        console.log("yanlish emeliyyat")
+    }
+}
+
+
+
+numberToText(a[0]);
+
+
+function numberTwiceText(v){
+  if (v == 1){
+    console.log("bir")
+  }
+  else if(v == 0){
+    console.log(" ")
+  }
+
+else if(v == 2){
+    console.log("iki")
+  }
+  else if(v == 3){
+    console.log("uc")
+  }
+  else if(v == 4){
+    console.log("dord")
+  }
+  else if(v == 5){
+    console.log("bes")
+  }
+
+  else if(v == 6){
+    console.log("alti")
+  }
+  else if(v == 7){
+    console.log("yeddi")
+  }
+  else if(v == 8){
+    console.log("sekkiz")
+  }
+  else if(v == 9){
+    console.log("doqquz")
+  }
+}
+
+numberTwiceText(a[1])
+
+
+
+
+
